5e3ac084cca4a9a95a3fc4dc2f68bd41eccb3d1f
Stephen Tjasink <stephen@tjasink.com>
2022-12-08 12:30:34 +0000

    Day 8

--- src/main/kotlin/Day08.kt
+++ src/main/kotlin/Day08.kt
@@ -11,80 +11,52 @@ fun main() {
 class Day08 : Solver {
 
     override fun part1(input: List<String>): Long {
-        val trees = mutableListOf<MutableList<Int>>()
-        val visible = mutableListOf<MutableList<Boolean>>()
-
-        input.forEach { row ->
-            trees.add(row.toCharArray().map { it.digitToInt() }.toMutableList())
-            visible.add(Array(row.length) {false} .toMutableList())
-        }
-
-        trees.forEachIndexed { rowNum, row ->
-            row.forEachIndexed { colNum, _ ->
+        val trees = input.map { row-> row.map { it.digitToInt() } }
+        return trees.mapIndexed { rowNum, row ->
+            row.mapIndexed { colNum, _ ->
                 if (colNum == 0 || rowNum == 0) {
-                    visible[rowNum][colNum] = true
+                    true
                 } else if (colNum == row.size - 1 || rowNum == trees[0].size - 1) {
-                    visible[rowNum][colNum] = true
+                    true
                 } else {
+                    val thisCol = trees.map { row -> row[colNum] }
                     val leftTrees = trees[rowNum].subList(0, colNum)
                     val rightTrees = trees[rowNum].subList(colNum+1, trees[rowNum].size)
-                    val thisCol = trees.map { row -> row[colNum] }
                     val topTrees = thisCol.subList(0, rowNum)
                     val bottomTrees = thisCol.subList(rowNum+1, thisCol.size)
                     if (trees[rowNum][colNum] > leftTrees.max()) {
-                        visible[rowNum][colNum] = true
+                        true
                     } else if (trees[rowNum][colNum] > rightTrees.max()) {
-                        visible[rowNum][colNum] = true
+                        true
                     } else if (trees[rowNum][colNum] > topTrees.max()) {
-                        visible[rowNum][colNum] = true
+                        true
                     } else if (trees[rowNum][colNum] > bottomTrees.max()) {
-                        visible[rowNum][colNum] = true
+                        true
+                    } else {
+                        false
                     }
                 }
-            }
-        }
-
-        val total = visible.sumOf { row ->
-            row.count { it }
-        }.toLong()
-
-        return total
+            }.count { it }
+        }.sum().toLong()
     }
 
     override fun part2(input: List<String>): Long {
-        val trees = mutableListOf<MutableList<Int>>()
-        val scores = mutableListOf<MutableList<Long>>()
-
-        input.forEach { row ->
-            trees.add(row.toCharArray().map { it.digitToInt() }.toMutableList())
-            scores.add(Array(row.length) {0L}.toMutableList())
-        }
-
-        trees.forEachIndexed { rowNum, row ->
-            row.forEachIndexed { colNum, tree ->
-                val leftTrees = trees[rowNum].subList(0, colNum).reversed()
-                val rightTrees = trees[rowNum].subList(colNum+1, trees[rowNum].size)
+        val trees = input.map { row-> row.map { it.digitToInt() } }
+        return trees.mapIndexed { rowNum, row ->
+            row.mapIndexed { colNum, tree ->
                 val thisCol = trees.map { row -> row[colNum] }
-                val topTrees = thisCol.subList(0, rowNum).reversed()
-                val bottomTrees = thisCol.subList(rowNum+1, thisCol.size)
-                val leftScore = findScore(tree, leftTrees)
-                val rightScore = findScore(tree, rightTrees)
-                val bottomScore = findScore(tree, bottomTrees)
-                val topScore = findScore(tree, topTrees)
-                scores[rowNum][colNum] = leftScore * rightScore * topScore * bottomScore
-            }
-        }
-
-        val total = scores.maxOf { row ->
-            row.max()
-        }
-
-        return total
+                val leftScore = trees[rowNum].subList(0, colNum).reversed().findScore(tree)
+                val rightScore = trees[rowNum].subList(colNum+1, trees[rowNum].size).findScore(tree)
+                val topScore = thisCol.subList(0, rowNum).reversed().findScore(tree)
+                val bottomScore = thisCol.subList(rowNum+1, thisCol.size).findScore(tree)
+                leftScore * rightScore * topScore * bottomScore
+            }.max()
+        }.max()
     }
 
-    private fun findScore(tree: Int, list: List<Int>): Long {
+    private fun List<Int>.findScore(tree: Int): Long {
         var count = 0L
-        list.forEach { otherTree ->
+        forEach { otherTree ->
             if (otherTree < tree) {
                 count += 1
             } else {
