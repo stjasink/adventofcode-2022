9cd8d450d55363d529a5dfda32608df0e1cf49fb
Stephen Tjasink <stephen@tjasink.com>
2022-12-16 09:13:55 +0000

    Day 16 not working yet

---
+++ src/main/kotlin/Day16.kt
@@ -0,0 +1,95 @@
+import common.Solver
+import common.runAndTime
+import common.loadInput
+
+fun main() {
+    val input = loadInput("day-16.txt")
+    val solver = Day16()
+    runAndTime(solver, input)
+}
+
+class Day16 : Solver {
+
+    override fun part1(input: List<String>): Long {
+        val valves = parseInput(input)
+        val routes = findRoutes(valves)
+//        val testRoute = listOf(valves["AA"]!!, valves["DD"]!!, valves["DD"]!!.open(), valves["CC"]!!, valves["BB"]!!, valves["BB"]!!.open(), valves["AA"]!!, valves["II"]!!, valves["JJ"]!!, valves["JJ"]!!.open(), valves["II"]!!, valves["AA"]!!, valves["DD"]!!.open(), valves["EE"]!!, valves["FF"]!!, valves["GG"]!!, valves["HH"]!!, valves["HH"]!!.open(), valves["GG"]!!, valves["FF"]!!, valves["EE"]!!, valves["EE"]!!.open(), valves["DD"]!!, valves["CC"]!!, valves["CC"]!!.open(), valves["CC"]!!, valves["CC"]!!, valves["CC"]!!, valves["CC"]!!, valves["CC"]!!.open(), valves["CC"]!!)
+//        val routes = listOf(testRoute)
+        val routeFlows = routes.map { flowForRoute(it) }
+        return routeFlows.max()
+    }
+
+    override fun part2(input: List<String>): Long {
+        return 0L
+    }
+
+    private fun flowForRoute(route: List<Valve>): Long {
+        val seenOpenValves = mutableSetOf<String>()
+        val flowAndTimes = route.mapIndexed { time, valve ->
+            val flowTime = if (valve.open && !seenOpenValves.contains(valve.name)) {
+                seenOpenValves.add(valve.name)
+                30 - time
+            } else {
+                0
+            }
+            valve.flow to flowTime
+        }
+        val totalFlow = flowAndTimes.map { (flow, time) ->
+            flow * time
+        }.sum()
+        return totalFlow.toLong()
+    }
+
+    private fun findRoutes(valves: Map<String, Valve>): List<List<Valve>> {
+        val completedRoutes = mutableListOf<List<Valve>>()
+        val currentRoute = mutableListOf<Valve>()
+
+        fun takeTurn(atValve: Valve) {
+            currentRoute.add(atValve)
+            if (currentRoute.size == 31) {
+                // time is up, so route is complete
+                // it's 31 total because we started with AA on time 0
+                completedRoutes.add(currentRoute)
+                currentRoute.removeLast()
+            } else {
+                // options are to open valve if not already open, or take exit
+                if (!atValve.open) {
+                    // open the valve
+                    takeTurn(atValve.open())
+                } else {
+                    // take each exit in turn
+                    atValve.exits.forEach { exit ->
+                        takeTurn(valves[exit]!!)
+                    }
+                    currentRoute.removeLast()
+                }
+            }
+        }
+
+        takeTurn(valves["AA"]!!)
+        return completedRoutes
+    }
+
+    private fun parseInput(input: List<String>): Map<String, Valve> {
+        val valves = mutableMapOf<String, Valve>()
+        val regex = Regex("Valve (.+) has flow rate=(.+); tunnel(s)? lead(s)? to valve(s)? (.+)")
+        input.forEach { line ->
+            val matches = regex.matchEntire(line)!!.groupValues
+            val name = matches[1]
+            val flow = matches[2].toInt()
+            val exits = matches[6].split(",").map { it.trim() }
+            val valve = Valve(name, flow, exits)
+            valves[name] = valve
+        }
+        return valves
+    }
+
+    data class Valve(
+        val name: String,
+        val flow: Int,
+        val exits: List<String>,
+        val open: Boolean = false
+    ) {
+        fun open() = copy(open = true)
+    }
+}
---
+++ src/test/kotlin/Day16Test.kt
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class Day16Test {
+
+    @Test
+    fun part1Test() {
+        val input = """
+            Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+            Valve BB has flow rate=13; tunnels lead to valves CC, AA
+            Valve CC has flow rate=2; tunnels lead to valves DD, BB
+            Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+            Valve EE has flow rate=3; tunnels lead to valves FF, DD
+            Valve FF has flow rate=0; tunnels lead to valves EE, GG
+            Valve GG has flow rate=0; tunnels lead to valves FF, HH
+            Valve HH has flow rate=22; tunnel leads to valve GG
+            Valve II has flow rate=0; tunnels lead to valves AA, JJ
+            Valve JJ has flow rate=21; tunnel leads to valve II
+        """.trimIndent().split('\n')
+
+        val answer = Day16().part1(input)
+        assertEquals(1651L, answer)
+    }
+
+    @Test
+    fun part2Test() {
+        val input = """
+            Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+            Valve BB has flow rate=13; tunnels lead to valves CC, AA
+            Valve CC has flow rate=2; tunnels lead to valves DD, BB
+            Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+            Valve EE has flow rate=3; tunnels lead to valves FF, DD
+            Valve FF has flow rate=0; tunnels lead to valves EE, GG
+            Valve GG has flow rate=0; tunnels lead to valves FF, HH
+            Valve HH has flow rate=22; tunnel leads to valve GG
+            Valve II has flow rate=0; tunnels lead to valves AA, JJ
+            Valve JJ has flow rate=21; tunnel leads to valve II
+        """.trimIndent().split('\n')
+
+        val answer = Day16().part2(input)
+        assertEquals(0L, answer)
+    }
+}
